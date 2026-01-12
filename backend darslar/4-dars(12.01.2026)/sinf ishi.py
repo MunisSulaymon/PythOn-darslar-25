26dffab696cc59a7ce37b320ad3a32989771d856
MunisSulaymon <niyozovj6@gmail.com>
2026-01-12 02:42:14 -0800

    i have created the project

---
+++ backend darslar/4-dars(12.01.2026)/sinf ishi.py
@@ -0,0 +1,19 @@
+# Django
+# frontend ,backend , databasource
+#backend qismi boshlaymiz 
+#httml ,css ,videosini ko'rish
+# venv asbob uskunalar turadigan joylar 
+# pip install django 
+# django-admin startproject my_first_project  (bu proyekt yaratadi)
+# python manage.py startapp ' app name' (app yaratadi proyekt uchun)
+# python manage.py runserver (serverni run qiladi)
+# python manage.py createsuperuser (admin akkaunt yaratadi)
+# python manage.py makemigrations (table lar yaratadi)
+# python manage.py migrate (DB da table yaratadi)
+# python manage.py collectstatic (statik fayllarni joylashtiradi)
+
+
+
+
+
+
